12b07381eae3185e4f8fe3e58c0924197aeaaada
Jarom Wilcox <wiljar00@users.noreply.github.com>
2025-01-08 15:35:58 -0700

    Add chalkraUI config. App runs successfully.

---
+++ src/components/ui/demo.jsx
@@ -0,0 +1,10 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+const Demo = () => {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  )
+}
