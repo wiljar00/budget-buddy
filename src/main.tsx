5e05d93710bf047701e1b097326977a93d4ca881
Jarom Wilcox <wiljar00@users.noreply.github.com>
2025-01-13 14:48:48 -0700

    add emotion/react to project

--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,35 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
+import { Global } from "@emotion/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider
       theme={{
-        // Define your theme overrides here
         colors: {
-          // Example: custom color
-          brand: ["#f0f", "#0ff", "#ff0"],
-        },
-      }}
-      styles={{
-        // Global styles
-        global: {
-          body: {
-            backgroundColor: "#f0f0f0",
-          },
+          brand: [
+            "#f0f",
+            "#0ff",
+            "#ff0",
+            "#f0f",
+            "#0ff",
+            "#ff0",
+            "#f0f",
+            "#0ff",
+            "#ff0",
+            "#f0f",
+          ],
         },
       }}
     >
+      <Global
+        styles={{
+          body: {
+            backgroundColor: "#f0f0f0",
+          },
+        }}
+      />
       <App />
     </MantineProvider>
   </StrictMode>
