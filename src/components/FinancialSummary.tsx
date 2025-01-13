4649c6dcc0efa23f9e7cfb12cdf0d433afcd0c9f
Jarom Wilcox <wiljar00@users.noreply.github.com>
2025-01-13 15:09:49 -0700

    Add mobile view of financial summary

--- src/components/FinancialSummary.tsx
+++ src/components/FinancialSummary.tsx
@@ -1,7 +1,11 @@
 import { Grid, Container } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import { useMediaQuery } from "@mantine/hooks";
 import FinancialCard from "./FinancialCard";
 
 export default function FinancialSummary() {
+  const isMobile = useMediaQuery("(max-width: 48em)");
+
   return (
     <Container size="md">
       <Grid gutter="md">
@@ -15,23 +19,53 @@ export default function FinancialSummary() {
           />
         </Grid.Col>
 
-        <Grid.Col span={{ base: 12, sm: 6 }}>
-          <FinancialCard
-            title="Expenses"
-            amount="$2,540.00"
-            description="Total expenses this month"
-            color="red.7"
-          />
-        </Grid.Col>
-
-        <Grid.Col span={{ base: 12, sm: 6 }}>
-          <FinancialCard
-            title="Income"
-            amount="$4,000.00"
-            description="Total income this month"
-            color="green.7"
-          />
-        </Grid.Col>
+        {isMobile ? (
+          <Grid.Col span={12}>
+            <Carousel
+              slideSize="100%"
+              height={150}
+              align="start"
+              slideGap="md"
+              withIndicators
+            >
+              <Carousel.Slide>
+                <FinancialCard
+                  title="Expenses"
+                  amount="$2,540.00"
+                  description="Total expenses this month"
+                  color="red.7"
+                />
+              </Carousel.Slide>
+              <Carousel.Slide>
+                <FinancialCard
+                  title="Income"
+                  amount="$4,000.00"
+                  description="Total income this month"
+                  color="green.7"
+                />
+              </Carousel.Slide>
+            </Carousel>
+          </Grid.Col>
+        ) : (
+          <>
+            <Grid.Col span={6}>
+              <FinancialCard
+                title="Expenses"
+                amount="$2,540.00"
+                description="Total expenses this month"
+                color="red.7"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <FinancialCard
+                title="Income"
+                amount="$4,000.00"
+                description="Total income this month"
+                color="green.7"
+              />
+            </Grid.Col>
+          </>
+        )}
       </Grid>
     </Container>
   );
