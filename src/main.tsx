e2342a34e30fe19b28c8c44d5ec42899b22e5161
Jarom Wilcox <wiljar00@users.noreply.github.com>
2025-01-13 14:43:31 -0700

    App is fully running with mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider
+      theme={{
+        // Define your theme overrides here
+        colors: {
+          // Example: custom color
+          brand: ["#f0f", "#0ff", "#ff0"],
+        },
+      }}
+      styles={{
+        // Global styles
+        global: {
+          body: {
+            backgroundColor: "#f0f0f0",
+          },
+        },
+      }}
+    >
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);
