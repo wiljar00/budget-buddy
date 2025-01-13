12cc7eee708c040388d76b5166cb2668ba633412
Jarom Wilcox <wiljar00@users.noreply.github.com>
2025-01-13 14:55:37 -0700

    Add icons and navbar to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import "./App.css";
+import { AppShell } from "@mantine/core";
+import Navbar from "./components/Navbar";
 import CardExample from "./components/card_example";
 
 function App() {
   return (
-    <>
-      <CardExample />
-    </>
+    <AppShell header={{ height: 60 }} padding="md">
+      <Navbar />
+      <AppShell.Main>
+        <CardExample />
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { AppShell, Group, Title, ActionIcon } from "@mantine/core";
+import { IconUser } from "@tabler/icons-react";
+
+export default function Navbar() {
+  return (
+    <AppShell.Header>
+      <Group h="100%" px="md" justify="space-between">
+        <Title order={1}>Norway Fjord Adventures</Title>
+        <ActionIcon variant="subtle" size="lg">
+          <IconUser />
+        </ActionIcon>
+      </Group>
+    </AppShell.Header>
+  );
+}
