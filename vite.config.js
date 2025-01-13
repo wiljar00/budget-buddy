6c8149743f058bd332eb48f0786ec1a18a6b1c30
Jarom Wilcox <wiljar00@users.noreply.github.com>
2025-01-13 14:33:22 -0700

    Resetting project to Vite basics
    ChalkraUI is causing issues. Switching direction to Mantine component library

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import jsconfigPaths from "vite-jsconfig-paths";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), jsconfigPaths()],
-});
